10fffcbef7dd0c168953fe7ab4217d9e655a681c
JodiAB <jodiabrahams9@gmail.com>
2024-02-26 16:07:34 +0200

    Back

---
+++ backend/controller/JewelleryController.js
@@ -0,0 +1,53 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import { jewellery } from '../model/index.js';
+
+const jewelleryRouter = express.Router();
+
+jewelleryRouter.get('/', (req, res)=>{
+    try {
+        jewellery.fetchJewellery(req, res);
+    } catch(e){
+        res.json({
+            status: res.statusCode,
+            msg: "Failed to retrieve jewellery items."
+        })
+    }
+})
+
+jewelleryRouter.get('/:id', (req, res)=>{
+    try {
+        jewellery.fetchJewellery(req, res);
+    } catch(e){
+        res.json({
+            status: res.statusCode,
+            msg: "Failed to retrieve jewellery items."
+        })
+    }
+})
+
+jewelleryRouter.post('/addJewel', bodyParser.json(), (req, res)=>{
+    try {
+        jewellery.addJewel(req, res);
+    } catch(e) {
+        res.json({
+            status: res.statusCode,
+            msg: "Failed to add jewellery items."
+        })
+    }
+})
+
+jewelleryRouter.patch('/update/:id', bodyParser.json(), (req, res)=>{
+    try {
+        jewellery.updateJewel(req, res);
+    } catch(e) {
+        res.json({
+            status: res.statusCode,
+            msg: "Failed to update jewellery items."
+        })
+    }
+})
+
+export {
+    jewelleryRouter
+}
